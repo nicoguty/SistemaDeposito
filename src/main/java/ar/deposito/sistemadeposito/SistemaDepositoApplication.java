4be0cbca52eb2e2af539446abb5ba923595166af
Nicolas Gutierrez <nicolas.gutierrez27@gmail.com>
2022-03-18 19:25:04 -0300

    First Commit

---
+++ src/main/java/ar/deposito/sistemadeposito/SistemaDepositoApplication.java
@@ -0,0 +1,13 @@
+package ar.deposito.sistemadeposito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDepositoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDepositoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/deposito/sistemadeposito/SistemaDepositoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.deposito.sistemadeposito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDepositoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
